51068e91c57069d36d40738542491199fc57c3a7
alilinykh <aililnykh@dtcc.edu>
2019-07-08 21:49:29 -0400

    commit

--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -2,10 +2,7 @@ package io.zipcoder;
 
 import io.zipcoder.utils.Item;
 import io.zipcoder.utils.ItemParseException;
-import io.zipcoder.utils.match.Match;
-import javafx.scene.paint.Material;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -17,57 +14,111 @@ public class ItemParser {
         List<Item> rslt = new ArrayList<>();
         List<String> sepItems = new ArrayList<>();
 
-        final String regex = "[^\\##]+";
-        final Pattern pattern = Pattern.compile(regex);
-        final Matcher matcher = pattern.matcher(valueToParse);
+        String regex = "[^##]+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(valueToParse);
 
-        sepItems.add(matcher.group(0));
-        for (int i = 1; i < matcher.groupCount(); i++) {
-            sepItems.add(matcher.group(i));
+        while (matcher.find()) {
+            sepItems.add(matcher.group(0));
         }
-
-//        String[] itemValues = valueToParse.split("(\\##(?!$))");
-//        for (String s: itemValues
-//             ) {
-//            String[] pairs = s.split("(\\;(!?))");
-//            for (String s1: pairs
-//                 ) {
-////                rslt.add(parseSingleItem(s1));
-//            }
-//        }
-
-
-
+        for ( String s : sepItems
+             ) {
+            try {
+                rslt.add(parseSingleItem(s));
+            } catch (ItemParseException e) {
+                e.printStackTrace();
+            }
+        }
 
 
         return rslt;
     }
 
     public Item parseSingleItem(String singleItem) throws ItemParseException {
-        List<String>keyValuePairs = new ArrayList<>();
-        final String regex1 = "[^\\;]+";
-        final Pattern pattern1 = Pattern.compile(regex1);
+        String string = replaceValuesToAt(singleItem);
+        String nameValue = null;
+        Double priceValue = null;
+        String typeValue = null;
+        String expirationValue = null;
+        List<String> keyValuePairs = new ArrayList<>();
 
-        Matcher matcher = pattern1.matcher(singleItem);
+        String regex1 = "[^;]+";
+        Pattern pattern1 = Pattern.compile(regex1);
+        Matcher matcher = pattern1.matcher(string);
         while (matcher.find()) {
             keyValuePairs.add(matcher.group(0));
         }
-        for (String s: keyValuePairs
-             ) {
-            final String reg = "([^\\W]*) + (([\\w]*)$)";
-            final Pattern pa = Pattern.compile(reg);
+
+        for (String s : keyValuePairs
+        ) {
+            String reg = "[^@]*";
+            Pattern pa = Pattern.compile(reg);
             Matcher matcher1 = pa.matcher(s);
+
             while (matcher1.find()) {
-                if (matcher1.group(0).toLowerCase().equals("name")) {
-                    String name = matcher1.group(1);
+                switch (matcher1.group(0).toLowerCase()) {
+                    case "name": {
+                        nameValue = getInfo(s);
+                        if (nameValue == null) {throw new ItemParseException();}
+                        break;
+                    }
+                    case "price": {
+                        priceValue = Double.parseDouble(getInfo(s));
+                        if (priceValue == null) {throw new ItemParseException();}
+                        break;
+                    }
+                    case "type": {
+                        typeValue = getInfo(s);
+                        if (typeValue == null) {throw new ItemParseException();}
+                        break;
+                    }
+                    case "expiration": {
+                        expirationValue = getInfo(s);
+//                        if (expirationValue.contains("##")
+                        if (expirationValue == null) {throw new ItemParseException();}
+                        break;
+                    }
                 }
             }
-
-
-
-
         }
 
+
+        return new Item(nameValue, priceValue, typeValue, expirationValue);
+    }
+
+    public static String replaceValuesToAt(String string) {
+        String regex = "[:|*|^|%]";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(string);
+        while (matcher.find()) {
+            string = matcher.replaceAll("@");
+        }
+        return string;
+    }
+
+
+    public static String getInfo(String string) {
+
+        final String reg = "@(.*)"; //  -----  ([\w]*)$
+        final Pattern pa = Pattern.compile(reg);
+        Matcher matcher1 = pa.matcher(string);
+        while (matcher1.find()) return matcher1.group(1).toLowerCase();
         return null;
     }
+//
+//    public static Double getPrice(String string) {
+//        final String reg = ""; //  -----  ([\w]*)$
+//        final Pattern pa = Pattern.compile(reg);
+//        Matcher matcher1 = pa.matcher(string);
+//        while (matcher1.find()) return matcher1.group(0);
+//        return null;
+//    }
+//
+//    public static String getExpiration(String string) {
+//        final String reg = "[\\w]*$"; //  -----  ([\w]*)$
+//        final Pattern pa = Pattern.compile(reg);
+//        Matcher matcher1 = pa.matcher(string);
+//        while (matcher1.find()) return matcher1.group(0).toLowerCase();
+//        return null;
+//    }
 }
